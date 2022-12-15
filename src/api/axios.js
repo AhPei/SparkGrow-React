c9a02fb6a8633e5beb4fcec93b733444ce9dc65c
AhPei <gohpeisheng20@gmail.com>
2022-12-15 20:57:16 +0800

    update base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -19,7 +19,8 @@ function getCookie(name) {
 }
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://sparkgrow.azurewebsites.net/',
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
