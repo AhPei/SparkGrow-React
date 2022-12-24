c7d09a0487afc79aad85d2ebab573181e7219584
AhPei <gohpeisheng20@gmail.com>
2022-12-24 16:28:07 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://sparkgrow.azurewebsites.net/",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
